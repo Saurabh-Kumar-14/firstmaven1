9e1e34e5e7cd05dfb87cb00803700c6d1a9f4ddd
SaurabhK <saurabh.kumar@navis.com>
2018-08-20 17:45:58 +0530

    new Project

---
+++ src/main/java/com/navis/core/App.java
@@ -0,0 +1,13 @@
+package com.navis.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "First Git project commit test" );
+    }
+}
